7263d6cc7a349375a0d95409dd0de63bfa7f08db
charlie <chacha0932@gmail.com>
2025-02-10 15:57:18 +0800

    新增: Log 檔案檢查以及輸出 illegal card 訊息

---
+++ app.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+
+const LOG_DIR = './checkLog'
+const OUTPUT_DIR = './output'
+
+if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+}
+
+const logFiles = fs.readdirSync(LOG_DIR)
+
+logFiles.forEach(logFile => {
+    try {
+        const outputPath = path.join(OUTPUT_DIR, logFile.replace('.log', '-illegalCard.json'))
+        const logPath = path.join(LOG_DIR, logFile)
+
+        console.log('Now is handling:', logFile, '...')
+
+        //TIPS: translate log filet to json
+        const logData = fs.readFileSync(logPath, 'utf8')
+        const jsonLog = logData.trim().split('\n').map(line => JSON.parse(line))
+
+
+        //TIPS: for check illegal card
+        const illegalCards = []
+        for (let i = 0; i < jsonLog.length; i++) {
+            if (jsonLog[i].message === 'illegal cards') {
+                const errDetail = {}
+                errDetail.Uid = jsonLog[i - 1].message.match(/\[(\d+)\]/)[1]
+                errDetail.message = jsonLog[i].message
+                errDetail.time = jsonLog[i].timestamp
+                illegalCards.push(errDetail)
+            }
+        }
+
+        fs.writeFileSync(outputPath, JSON.stringify(illegalCards, null, 2))
+
+        console.log('Log file check done:', logFile)
+
+    } catch (error) {
+        console.log('Transform log file failed:', error)
+    }
+})
