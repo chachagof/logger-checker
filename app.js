5f4af70d9b4c03a3a7dc73eb3b433a893b062625
charlie <chacha0932@gmail.com>
2025-02-11 18:28:57 +0800

    修改: 引用不同錯誤處理並調整輸出資料夾

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const IllegalCardsController = require('./controller/illegalCards')
+const normalErrorController = require('./controller/normalError')
+const fs = require('fs')
 
 const LOG_DIR = './checkLog'
 const OUTPUT_DIR = './output'
+const ILLEGAL_OUTPUT_DIR = './output/IllegalOutput'
+const NORMAL_OUTPUT_DIR = './output/normalOutput'
 
-IllegalCardsController.logFilterIllegalCardsByDate(LOG_DIR, OUTPUT_DIR)
+if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+}
+
+IllegalCardsController.logFilterIllegalCardsByDate(LOG_DIR, ILLEGAL_OUTPUT_DIR)
+normalErrorController.logFilterNormalErrorByDate(LOG_DIR, NORMAL_OUTPUT_DIR)
