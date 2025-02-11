765866199f5dc277d43b2ec7110ef03eae00e901
charlie <chacha0932@gmail.com>
2025-02-11 16:14:11 +0800

    修改: 拆分專案統計錯誤資訊邏輯(log filter by date)

--- app.js
+++ app.js
@@ -1,49 +1,6 @@
-const fs = require('fs')
-const path = require('path')
 const IllegalCardsController = require('./controller/illegalCards')
 
 const LOG_DIR = './checkLog'
 const OUTPUT_DIR = './output'
-const totalIllegalCards = []
 
-if (!fs.existsSync(OUTPUT_DIR)) {
-    fs.mkdirSync(OUTPUT_DIR)
-}
-
-const logFiles = fs.readdirSync(LOG_DIR)
-
-logFiles.forEach(logFile => {
-    try {
-        const outputPath = path.join(OUTPUT_DIR, logFile.replace('.log', '-illegalCard.json'))
-        const logPath = path.join(LOG_DIR, logFile)
-
-        console.log('Now is handling:', logFile, '...')
-
-        //TIPS: translate log filet to json
-        const logData = fs.readFileSync(logPath, 'utf8')
-        const jsonLog = logData.trim().split('\n').map(line => JSON.parse(line))
-
-
-        //TIPS: for check illegal card
-        const illegalCards = []
-        for (let i = 0; i < jsonLog.length; i++) {
-            if (jsonLog[i].message === 'illegal cards') {
-                const errDetail = {}
-                errDetail.Uid = jsonLog[i - 1].message.match(/\[(\d+)\]/)[1]
-                errDetail.message = jsonLog[i].message
-                errDetail.time = jsonLog[i].timestamp
-                illegalCards.push(errDetail)
-                totalIllegalCards.push(errDetail)
-            }
-        }
-
-        fs.writeFileSync(outputPath, JSON.stringify(illegalCards, null, 2))
-
-        console.log('Log file check done:', logFile)
-
-    } catch (error) {
-        console.log('Transform log file failed:', error)
-    }
-})
-
-IllegalCardsController.filterIllegalCards(totalIllegalCards, OUTPUT_DIR)
+IllegalCardsController.logFilterIllegalCardsByDate(LOG_DIR, OUTPUT_DIR)
--- controller/illegalCards.js
+++ controller/illegalCards.js
@@ -2,8 +2,52 @@ const path = require('path')
 const fs = require('fs')
 
 const IllegalCardsController = {
-    //@param LogData:[{Uid:string, message:string, time:string}]
-    filterIllegalCards: (logData, OUTPUT_DIR) => {
+    logFilterIllegalCardsByDate: (LOG_DIR, OUTPUT_DIR) => {
+        const totalIllegalCards = []
+
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+
+        const logFiles = fs.readdirSync(LOG_DIR)
+
+        logFiles.forEach(logFile => {
+            try {
+                const outputPath = path.join(OUTPUT_DIR, logFile.replace('.log', '-illegalCard.json'))
+                const logPath = path.join(LOG_DIR, logFile)
+
+                console.log('Now is handling:', logFile, '...')
+
+                //TIPS: translate log filet to json
+                const logData = fs.readFileSync(logPath, 'utf8')
+                const jsonLog = logData.trim().split('\n').map(line => JSON.parse(line))
+
+
+                //TIPS: for check illegal card
+                const illegalCards = []
+                for (let i = 0; i < jsonLog.length; i++) {
+                    if (jsonLog[i].message === 'illegal cards') {
+                        const errDetail = {}
+                        errDetail.Uid = jsonLog[i - 1].message.match(/\[(\d+)\]/)[1]
+                        errDetail.message = jsonLog[i].message
+                        errDetail.time = jsonLog[i].timestamp
+                        illegalCards.push(errDetail)
+                        totalIllegalCards.push(errDetail)
+                    }
+                }
+
+                fs.writeFileSync(outputPath, JSON.stringify(illegalCards, null, 2))
+
+                console.log('Log file check done:', logFile)
+
+            } catch (error) {
+                console.log('Transform log file failed:', error)
+            }
+        })
+
+        IllegalCardsController.calculateTotalIllegalCards(totalIllegalCards, OUTPUT_DIR)
+    },
+    calculateTotalIllegalCards: (logData, OUTPUT_DIR) => {
         const outputTotalPath = path.join(OUTPUT_DIR, 'totalIllegalCardsRecord.json')
         const filterIllegalCards = logData.reduce((acc, cur) => {
             if (!acc[cur.Uid]) {
